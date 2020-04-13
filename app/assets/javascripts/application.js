bd6b7121c3e7efb32368d89e71d4098eed0e6373
Lucas Siqueira Fernandes <lucassiqueirafernandes07@gmail.com>
2020-04-13 14:07:05 -0300

    changing jquery navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,5 @@
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require materialize-sprockets
 //
 // Select2
@@ -16,9 +15,9 @@
 $( document ).on('turbolinks:load', function() {
 	// Navbar
 	$(".click_navbar .close").hide();
-	$("nav").hide();
+	$(".nav_trainer").hide();
 	$(".click_navbar").click(function() {
-	  $("nav").toggle(300);
+	  $(".nav_trainer").toggle(300);
 	  $(".click_navbar .close").toggle(200);
 	  $(".click_navbar .open").toggle(200);
 	});
