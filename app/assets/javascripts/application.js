cdd3604bb034379c6b6340a6578c77a1020ed990
Lucas Siqueira Fernandes <lucassiqueirafernandes07@gmail.com>
2020-04-13 14:12:22 -0300

    changing jquery navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,16 +12,3 @@
 //
 //= require serviceworker-companion
 //= require_tree .
-
-$( document ).on('turbolinks:load', function() {
-	// Navbar
-	$(".click_navbar .close").hide();
-	$(".nav_trainer").hide();
-	$(".click_navbar").click(function() {
-	  $(".nav_trainer").toggle(300);
-	  $(".click_navbar .close").toggle(200);
-	  $(".click_navbar .open").toggle(200);
-	});
-
-	$(".trainer_card").hide();
-});
