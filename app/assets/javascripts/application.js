2eed7021e2a2425015dac345afbe0757238e722f
Lucas Siqueira Fernandes <lucassiqueirafernandes07@gmail.com>
2020-04-13 13:27:57 -0300

    changing jquery navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,11 +14,13 @@
 //= require_tree .
 
 
-// Navbar
-$('.click_navbar .close').hide();
-$('.nav_trainer').hide();
-$('.click_navbar').click(function() {
-  $('.nav_trainer').toggle(300);
-  $('.click_navbar .close').toggle(200);
-  $('.click_navbar .open').toggle(200);
+$( document ).ready(function() {
+	// Navbar
+	$('.click_navbar .close').hide();
+	$('.nav_trainer').hide();
+	$('.click_navbar').click(function() {
+	  $('.nav_trainer').toggle(300);
+	  $('.click_navbar .close').toggle(200);
+	  $('.click_navbar .open').toggle(200);
+	});
 });
