3228a9ba1faf216d497caafc24adf8b1ca2329af
Lucas Siqueira Fernandes <lucassiqueirafernandes07@gmail.com>
2020-04-13 00:01:08 -0300

    reformulating views and pupil registration

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,6 @@
 class WelcomeController < ApplicationController
 	def index
-		@weight = Weight.new
-
+		@weight = Weight.new if pupil_signed_in?
 		@weight_of_today = Weight.where("date(created_at) = ? AND pupil_id = ?", Date.today, current_pupil).last.value if pupil_signed_in?
 	end
 end
